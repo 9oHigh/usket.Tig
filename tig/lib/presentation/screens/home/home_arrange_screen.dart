b14ed7c5505078dec7ada61288951641297c803a
9oHigh <wkdska957@naver.com>
2024-09-18 22:56:52 +0900

    fix: remove monthly, weekly priority member variable

--- tig/lib/presentation/screens/home/home_arrange_screen.dart
+++ tig/lib/presentation/screens/home/home_arrange_screen.dart
@@ -2,15 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-enum ArrangeType { isOnMonthly, isOnWeekly, isOnDaily, isOnBraindump }
+enum ArrangeType { isOnDaily, isOnBraindump }
 
 extension on ArrangeType {
   String get arrangeName {
     switch (this) {
-      case ArrangeType.isOnMonthly:
-        return 'isOnMonthly';
-      case ArrangeType.isOnWeekly:
-        return 'isOnWeekly';
       case ArrangeType.isOnDaily:
         return 'isOnDaily';
       case ArrangeType.isOnBraindump:
@@ -27,8 +23,6 @@ class HomeArrangeScreen extends StatefulWidget {
 }
 
 class _HomeArrangeScreen extends State<HomeArrangeScreen> {
-  bool _isOnMonthly = false;
-  bool _isOnWeekly = false;
   bool _isOnDaily = false;
   bool _isOnBraindump = false;
 
@@ -41,8 +35,6 @@ class _HomeArrangeScreen extends State<HomeArrangeScreen> {
   void initailizeOptions() async {
     final SharedPreferences pref = await SharedPreferences.getInstance();
     setState(() {
-      _isOnMonthly = pref.getBool('isOnMonthly') ?? false;
-      _isOnWeekly = pref.getBool('isOnWeekly') ?? false;
       _isOnDaily = pref.getBool('isOnDaily') ?? false;
       _isOnBraindump = pref.getBool('isOnBraindump') ?? false;
     });
@@ -70,48 +62,6 @@ class _HomeArrangeScreen extends State<HomeArrangeScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Row(
-                children: [
-                  const Text('Monthly priority'),
-                  Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        CupertinoSwitch(
-                          value: _isOnMonthly,
-                          onChanged: (value) {
-                            setState(() {
-                              setPref(ArrangeType.isOnMonthly, value);
-                              _isOnMonthly = value;
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  const Text('Weekly priority'),
-                  Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        CupertinoSwitch(
-                          value: _isOnWeekly,
-                          onChanged: (value) {
-                            setState(() {
-                              setPref(ArrangeType.isOnWeekly, value);
-                              _isOnWeekly = value;
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
               Row(
                 children: [
                   const Text('Daily priority'),
