6ff2f52e846dcf47cb53159afd1fb6f524fdd0b6
9oHigh <wkdska957@naver.com>
2024-10-22 02:30:12 +0900

    feat: use flutter intl and flutter localization to apply multi-lang

---
+++ tig/lib/generated/intl/messages_ko.dart
@@ -0,0 +1,123 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ko locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ko';
+
+  static String m0(error) => "구글 로그인에 실패했습니다.\n오류: ${error}";
+
+  static String m1(error) => "카카오 로그인에 실패했습니다.\n오류: ${error}";
+
+  static String m2(error) => "앱을 다시 시작해 주세요.\n오류: ${error}";
+
+  static String m3(month) => "${month}월 Tigs";
+
+  static String m4(price) => "${price}/월";
+
+  static String m5(item) => "${item}가 삭제되었습니다.";
+
+  static String m6(minute, second) => "${minute}분 : ${second}초";
+
+  static String m7(endTime) => "종료 시간: ${endTime}";
+
+  static String m8(startTime) => "시작 시간: ${startTime}";
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "auth_google_login": MessageLookupByLibrary.simpleMessage("구글 로그인"),
+        "auth_google_login_failure": m0,
+        "auth_google_login_success":
+            MessageLookupByLibrary.simpleMessage("구글 로그인에 성공했습니다."),
+        "auth_kakao_login": MessageLookupByLibrary.simpleMessage("카카오 로그인"),
+        "auth_kakao_login_failure": m1,
+        "auth_kakao_login_success":
+            MessageLookupByLibrary.simpleMessage("카카오 로그인에 성공했습니다."),
+        "cancel": MessageLookupByLibrary.simpleMessage("취소"),
+        "delete": MessageLookupByLibrary.simpleMessage("삭제"),
+        "end": MessageLookupByLibrary.simpleMessage("종료"),
+        "end_time": MessageLookupByLibrary.simpleMessage("종료 시간"),
+        "exit": MessageLookupByLibrary.simpleMessage("나가기"),
+        "home_activity_placeholder":
+            MessageLookupByLibrary.simpleMessage("할 일을 적어 주세요."),
+        "home_arrange_title": MessageLookupByLibrary.simpleMessage("보기 옵션"),
+        "home_braindump_placeholder":
+            MessageLookupByLibrary.simpleMessage("지금 떠오르는 모든 생각들을 적어보세요!"),
+        "home_save_completed": MessageLookupByLibrary.simpleMessage("저장 완료"),
+        "home_save_completed_desc":
+            MessageLookupByLibrary.simpleMessage("티그들이 정상적으로 저장되었습니다."),
+        "home_save_desc": MessageLookupByLibrary.simpleMessage("저장하기"),
+        "home_tig_mode_prepare": MessageLookupByLibrary.simpleMessage(
+            "티그 모드를 위해 데이터를 준비 중입니다.\n준비되는 동안 광고가 표시됩니다.\n잠시만 기다려 주세요 🙏"),
+        "main_restart": m2,
+        "menu_contact_us": MessageLookupByLibrary.simpleMessage("문의하기"),
+        "menu_delete_user_failure": MessageLookupByLibrary.simpleMessage(
+            "오류가 발생했습니다.\n잠시 후 다시 시도해 주세요."),
+        "menu_email_body": MessageLookupByLibrary.simpleMessage(
+            "아래 내용을 함께 보내주시면 큰 도움이 됩니다.\n사용 중인 기기:\n오류 또는 개선 사항:\n\n추가 문의 사항이 있으시면 편하게 작성해 주세요 :)"),
+        "menu_email_subject": MessageLookupByLibrary.simpleMessage("문의드립니다."),
+        "menu_logout_content":
+            MessageLookupByLibrary.simpleMessage("로그아웃 하시겠습니까?"),
+        "menu_logout_text": MessageLookupByLibrary.simpleMessage("로그아웃"),
+        "menu_logout_title": MessageLookupByLibrary.simpleMessage("로그아웃 안내"),
+        "menu_month_tigs": m3,
+        "menu_price_per_month": m4,
+        "menu_subscribe": MessageLookupByLibrary.simpleMessage("구독하기"),
+        "menu_subscribe_cancel":
+            MessageLookupByLibrary.simpleMessage("구독 취소하기"),
+        "menu_subscribe_get1":
+            MessageLookupByLibrary.simpleMessage("구독하시면 모든 광고가 제거됩니다😊"),
+        "menu_subscribe_get2": MessageLookupByLibrary.simpleMessage(
+            "구독하시면 월별 및 일별 진행 상황을 확인할 수 있는\n위젯들을 제공합니다😊"),
+        "menu_subscribe_get3": MessageLookupByLibrary.simpleMessage(
+            "구독을 통해 더 멋진 앱을 만드는 데\n도움을 주실 수 있습니다.\n항상 감사합니다😊"),
+        "menu_subscribe_lose1":
+            MessageLookupByLibrary.simpleMessage("구독을 취소하면 광고가 다시 표시됩니다🥲"),
+        "menu_subscribe_lose2": MessageLookupByLibrary.simpleMessage(
+            "구독을 취소하면 진행 상황을 확인할 수 있는 위젯이 사라집니다🥲"),
+        "menu_subscribe_lose3": MessageLookupByLibrary.simpleMessage(
+            "구독을 취소하면 향후 추가될 기능을 사용할 수 없습니다🥲"),
+        "menu_withdrawal_content": MessageLookupByLibrary.simpleMessage(
+            "회원 탈퇴 시 모든 정보가 삭제됩니다.\n그래도 진행하시겠습니까?"),
+        "menu_withdrawal_text": MessageLookupByLibrary.simpleMessage("회원 탈퇴"),
+        "menu_withdrawal_title":
+            MessageLookupByLibrary.simpleMessage("회원 탈퇴 안내"),
+        "next": MessageLookupByLibrary.simpleMessage("다음"),
+        "ok": MessageLookupByLibrary.simpleMessage("확인"),
+        "setting": MessageLookupByLibrary.simpleMessage("설정"),
+        "success": MessageLookupByLibrary.simpleMessage("성공"),
+        "tag_add": MessageLookupByLibrary.simpleMessage("태그 추가"),
+        "tag_add_input": MessageLookupByLibrary.simpleMessage("추가할 태그를 입력하세요."),
+        "tag_delete_completed": m5,
+        "tag_delete_content":
+            MessageLookupByLibrary.simpleMessage("이 태그를 삭제하시겠습니까?"),
+        "tag_delete_title": MessageLookupByLibrary.simpleMessage("삭제 안내"),
+        "tag_duplicated":
+            MessageLookupByLibrary.simpleMessage("동일한 태그는 등록할 수 없습니다."),
+        "tag_empty": MessageLookupByLibrary.simpleMessage("추가할 태그를 입력해 주세요."),
+        "tag_title": MessageLookupByLibrary.simpleMessage("태그"),
+        "tig_mode": MessageLookupByLibrary.simpleMessage("티그 모드"),
+        "tig_mode_count_down": m6,
+        "tig_mode_empty_tig": MessageLookupByLibrary.simpleMessage(
+            "현재 시간에는 티그가 존재하지 않습니다.\n티그를 등록하고 다시 시작해 주세요😊"),
+        "tig_mode_end_time": m7,
+        "tig_mode_remain_time": MessageLookupByLibrary.simpleMessage("남은 시간"),
+        "tig_mode_start_time": m8,
+        "tig_mode_waiting": MessageLookupByLibrary.simpleMessage("대기 중")
+      };
+}
---
+++ tig/lib/generated/intl/messages_zh_CN.dart
@@ -0,0 +1,119 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a zh_CN locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'zh_CN';
+
+  static String m0(error) => "谷歌登录失败\n错误：${error}";
+
+  static String m1(error) => "Kakao 登录失败\n错误：${error}";
+
+  static String m2(error) => "请重新启动应用程序。\n错误：${error}";
+
+  static String m3(month) => "${month}月提格";
+
+  static String m4(price) => "${price}/月";
+
+  static String m5(item) => "${item} 已删除。";
+
+  static String m6(minute, second) => "${minute} : ${second}";
+
+  static String m7(endTime) => "结束时间：${endTime}";
+
+  static String m8(startTime) => "开始时间：${startTime}";
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "auth_google_login": MessageLookupByLibrary.simpleMessage("谷歌登录"),
+        "auth_google_login_failure": m0,
+        "auth_google_login_success":
+            MessageLookupByLibrary.simpleMessage("谷歌登录成功"),
+        "auth_kakao_login": MessageLookupByLibrary.simpleMessage("Kakao 登录"),
+        "auth_kakao_login_failure": m1,
+        "auth_kakao_login_success":
+            MessageLookupByLibrary.simpleMessage("Kakao 登录成功"),
+        "cancel": MessageLookupByLibrary.simpleMessage("取消"),
+        "delete": MessageLookupByLibrary.simpleMessage("删除"),
+        "end": MessageLookupByLibrary.simpleMessage("结束"),
+        "end_time": MessageLookupByLibrary.simpleMessage("结束时间"),
+        "exit": MessageLookupByLibrary.simpleMessage("退出"),
+        "home_activity_placeholder":
+            MessageLookupByLibrary.simpleMessage("请写下您的待办事项。"),
+        "home_arrange_title": MessageLookupByLibrary.simpleMessage("查看选项"),
+        "home_braindump_placeholder":
+            MessageLookupByLibrary.simpleMessage("写下您想到的所有想法！"),
+        "home_save_completed": MessageLookupByLibrary.simpleMessage("保存完成"),
+        "home_save_completed_desc":
+            MessageLookupByLibrary.simpleMessage("保存已完成。"),
+        "home_save_desc": MessageLookupByLibrary.simpleMessage("保存"),
+        "home_tig_mode_prepare": MessageLookupByLibrary.simpleMessage(
+            "正在为提格模式准备数据。\n准备过程中会显示广告。\n请稍等 🙏"),
+        "main_restart": m2,
+        "menu_contact_us": MessageLookupByLibrary.simpleMessage("联系我们"),
+        "menu_delete_user_failure":
+            MessageLookupByLibrary.simpleMessage("发生错误。\n请稍后重试。"),
+        "menu_email_body": MessageLookupByLibrary.simpleMessage(
+            "请提供以下信息以便我们帮助您。\n使用的设备：\n错误或改进意见：\n\n如果您有其他问题，请随时联系：)"),
+        "menu_email_subject": MessageLookupByLibrary.simpleMessage("咨询请求"),
+        "menu_logout_content": MessageLookupByLibrary.simpleMessage("您确定要退出吗？"),
+        "menu_logout_text": MessageLookupByLibrary.simpleMessage("注销"),
+        "menu_logout_title": MessageLookupByLibrary.simpleMessage("注销说明"),
+        "menu_month_tigs": m3,
+        "menu_price_per_month": m4,
+        "menu_subscribe": MessageLookupByLibrary.simpleMessage("订阅"),
+        "menu_subscribe_cancel": MessageLookupByLibrary.simpleMessage("取消订阅"),
+        "menu_subscribe_get1":
+            MessageLookupByLibrary.simpleMessage("订阅后，屏幕上的所有广告将被删除😊"),
+        "menu_subscribe_get2":
+            MessageLookupByLibrary.simpleMessage("订阅后，您将获得一个小部件，以查看每月和每日的进度😊"),
+        "menu_subscribe_get3":
+            MessageLookupByLibrary.simpleMessage("订阅将支持开发者，帮助我们创造更好的应用😊"),
+        "menu_subscribe_lose1":
+            MessageLookupByLibrary.simpleMessage("如果您取消订阅，广告将重新出现在屏幕上🥲"),
+        "menu_subscribe_lose2":
+            MessageLookupByLibrary.simpleMessage("如果您取消订阅，您将不再获得查看进度的小部件🥲"),
+        "menu_subscribe_lose3":
+            MessageLookupByLibrary.simpleMessage("如果您取消订阅，您将无法使用将来创建的功能🥲"),
+        "menu_withdrawal_content": MessageLookupByLibrary.simpleMessage(
+            "您取消会员资格时，所有信息将被删除。\n您是否确定要继续？"),
+        "menu_withdrawal_text": MessageLookupByLibrary.simpleMessage("取消会员资格"),
+        "menu_withdrawal_title": MessageLookupByLibrary.simpleMessage("退会信息"),
+        "next": MessageLookupByLibrary.simpleMessage("下一个"),
+        "ok": MessageLookupByLibrary.simpleMessage("确认"),
+        "setting": MessageLookupByLibrary.simpleMessage("设置"),
+        "success": MessageLookupByLibrary.simpleMessage("成功"),
+        "tag_add": MessageLookupByLibrary.simpleMessage("添加标签"),
+        "tag_add_input": MessageLookupByLibrary.simpleMessage("请输入要添加的标签。"),
+        "tag_delete_completed": m5,
+        "tag_delete_content":
+            MessageLookupByLibrary.simpleMessage("您确定要删除此标签吗？"),
+        "tag_delete_title": MessageLookupByLibrary.simpleMessage("删除说明"),
+        "tag_duplicated": MessageLookupByLibrary.simpleMessage("不能添加相同的标签。"),
+        "tag_empty": MessageLookupByLibrary.simpleMessage("请输入要添加的标签。"),
+        "tag_title": MessageLookupByLibrary.simpleMessage("标签"),
+        "tig_mode": MessageLookupByLibrary.simpleMessage("提格模式"),
+        "tig_mode_count_down": m6,
+        "tig_mode_empty_tig":
+            MessageLookupByLibrary.simpleMessage("当前没有提格。\n请注册提格并重新启动😊"),
+        "tig_mode_end_time": m7,
+        "tig_mode_remain_time": MessageLookupByLibrary.simpleMessage("剩余时间"),
+        "tig_mode_start_time": m8,
+        "tig_mode_waiting": MessageLookupByLibrary.simpleMessage("等待中")
+      };
+}
