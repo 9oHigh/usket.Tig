5796c9b93f965781b3cf6adb44d5ac3daadc7528
9oHigh <wkdska957@naver.com>
2025-02-01 14:30:16 +0900

    feat: update arb files for time system

--- tig/lib/generated/intl/messages_de.dart
+++ tig/lib/generated/intl/messages_de.dart
@@ -41,105 +41,145 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "auth_elon_musk": MessageLookupByLibrary.simpleMessage("Elon Musk "),
-        "auth_for_time_box_planner":
-            MessageLookupByLibrary.simpleMessage("Um Ihre Ziele zu erreichen,"),
-        "auth_google_login":
-            MessageLookupByLibrary.simpleMessage("Google-Login"),
-        "auth_google_login_failure": m0,
-        "auth_google_login_success": MessageLookupByLibrary.simpleMessage(
-            "Google-Anmeldung erfolgreich"),
-        "auth_kakao_login": MessageLookupByLibrary.simpleMessage("Kakao-Login"),
-        "auth_kakao_login_failure": m1,
-        "auth_kakao_login_success":
-            MessageLookupByLibrary.simpleMessage("Kakao-Anmeldung erfolgreich"),
-        "auth_palnner": MessageLookupByLibrary.simpleMessage("Planer."),
-        "auth_pick_best":
-            MessageLookupByLibrary.simpleMessage("wählte den besten "),
-        "auth_with_timebox_planner": MessageLookupByLibrary.simpleMessage(
-            "treten Sie dem TimeBox Planner bei."),
-        "cancel": MessageLookupByLibrary.simpleMessage("Abbrechen"),
-        "delete": MessageLookupByLibrary.simpleMessage("Löschen"),
-        "end": MessageLookupByLibrary.simpleMessage("Ende"),
-        "end_time": MessageLookupByLibrary.simpleMessage("Endzeit"),
-        "exit": MessageLookupByLibrary.simpleMessage("Beenden"),
-        "home_activity_placeholder": MessageLookupByLibrary.simpleMessage(
-            "Geben Sie eine Aktivität ein"),
-        "home_arrange_title":
-            MessageLookupByLibrary.simpleMessage("Ansichtsoptionen"),
-        "home_braindump_placeholder": MessageLookupByLibrary.simpleMessage(
-            "Schreiben Sie alle Gedanken auf, die Ihnen gerade durch den Kopf gehen!"),
-        "home_save_completed":
-            MessageLookupByLibrary.simpleMessage("Speichern abgeschlossen"),
-        "home_save_completed_desc": MessageLookupByLibrary.simpleMessage(
-            "Ihre Daten wurden erfolgreich gespeichert."),
-        "home_save_desc": MessageLookupByLibrary.simpleMessage("Speichern"),
-        "home_tig_mode_prepare": MessageLookupByLibrary.simpleMessage(
-            "Daten für den Tig-Modus werden vorbereitet.\nWährend der Vorbereitung wird eine Anzeige gezeigt.\nBitte warten Sie 🙏"),
-        "main_restart": m2,
-        "menu_contact_us":
-            MessageLookupByLibrary.simpleMessage("Kontaktieren Sie uns"),
-        "menu_delete_user_failure": MessageLookupByLibrary.simpleMessage(
-            "Ein Fehler ist aufgetreten.\nBitte versuchen Sie es später erneut."),
-        "menu_email_body": MessageLookupByLibrary.simpleMessage(
-            "Es wäre hilfreich, wenn Sie uns Folgendes mitteilen könnten:\nVerwendetes Gerät:\nProblem oder Verbesserungsvorschlag:\n\nFügen Sie gerne weitere Fragen hinzu :)"),
-        "menu_email_subject":
-            MessageLookupByLibrary.simpleMessage("Ich habe eine Frage"),
-        "menu_logout_content":
-            MessageLookupByLibrary.simpleMessage("Möchten Sie sich abmelden?"),
-        "menu_logout_text": MessageLookupByLibrary.simpleMessage("Abmelden"),
-        "menu_logout_title":
-            MessageLookupByLibrary.simpleMessage("Abmeldeanleitung"),
-        "menu_month_tigs": m3,
-        "menu_price_per_month": m4,
-        "menu_subscribe": MessageLookupByLibrary.simpleMessage("Abonnieren"),
-        "menu_subscribe_cancel":
-            MessageLookupByLibrary.simpleMessage("Abo kündigen"),
-        "menu_subscribe_get1": MessageLookupByLibrary.simpleMessage(
-            "Durch ein Abo werden alle Anzeigen entfernt 😊"),
-        "menu_subscribe_get2": MessageLookupByLibrary.simpleMessage(
-            "Sie können Widgets verwenden, um Ihren monatlichen und täglichen Fortschritt zu verfolgen 😊"),
-        "menu_subscribe_get3": MessageLookupByLibrary.simpleMessage(
-            "Sie unterstützen die Entwickler bei der Verbesserung der App 😊"),
-        "menu_subscribe_lose1": MessageLookupByLibrary.simpleMessage(
-            "Wenn Sie das Abo kündigen, werden die Anzeigen wieder erscheinen 🥲"),
-        "menu_subscribe_lose2": MessageLookupByLibrary.simpleMessage(
-            "Sie verlieren das Widget, das Ihren Fortschritt verfolgt 🥲"),
-        "menu_subscribe_lose3": MessageLookupByLibrary.simpleMessage(
-            "Sie verlieren den Zugriff auf kommende Funktionen 🥲"),
-        "menu_update_intro":
-            MessageLookupByLibrary.simpleMessage("Demnächst verfügbar."),
-        "menu_withdrawal_content": MessageLookupByLibrary.simpleMessage(
-            "Alle Ihre Daten werden gelöscht.\nMöchten Sie fortfahren?"),
-        "menu_withdrawal_text":
-            MessageLookupByLibrary.simpleMessage("Konto löschen"),
-        "menu_withdrawal_title":
-            MessageLookupByLibrary.simpleMessage("Kontolöschung"),
-        "next": MessageLookupByLibrary.simpleMessage("Weiter"),
-        "ok": MessageLookupByLibrary.simpleMessage("OK"),
-        "setting": MessageLookupByLibrary.simpleMessage("Einstellungen"),
-        "success": MessageLookupByLibrary.simpleMessage("Erfolg"),
-        "tag_add": MessageLookupByLibrary.simpleMessage("Tag hinzufügen"),
-        "tag_add_input":
-            MessageLookupByLibrary.simpleMessage("Geben Sie ein Tag ein"),
-        "tag_delete_completed": m5,
-        "tag_delete_content": MessageLookupByLibrary.simpleMessage(
-            "Sind Sie sicher, dass Sie dieses Tag löschen möchten?"),
-        "tag_delete_title":
-            MessageLookupByLibrary.simpleMessage("Löschbestätigung"),
-        "tag_duplicated": MessageLookupByLibrary.simpleMessage(
-            "Tag kann nicht doppelt hinzugefügt werden."),
-        "tag_empty":
-            MessageLookupByLibrary.simpleMessage("Bitte geben Sie ein Tag ein"),
-        "tag_title": MessageLookupByLibrary.simpleMessage("Tags"),
-        "tig_mode": MessageLookupByLibrary.simpleMessage("Tig-Modus"),
-        "tig_mode_count_down": m6,
-        "tig_mode_empty_tig": MessageLookupByLibrary.simpleMessage(
-            "Zurzeit gibt es keine Tigs.\nVersuchen Sie, eines hinzuzufügen, und starten Sie erneut 😊"),
-        "tig_mode_end_time": m7,
-        "tig_mode_remain_time":
-            MessageLookupByLibrary.simpleMessage("Verbleibende Zeit"),
-        "tig_mode_start_time": m8,
-        "tig_mode_waiting": MessageLookupByLibrary.simpleMessage("Warten")
-      };
+    "auth_elon_musk": MessageLookupByLibrary.simpleMessage("Elon Musk "),
+    "auth_for_time_box_planner": MessageLookupByLibrary.simpleMessage(
+      "Um Ihre Ziele zu erreichen,",
+    ),
+    "auth_google_login": MessageLookupByLibrary.simpleMessage("Google-Login"),
+    "auth_google_login_failure": m0,
+    "auth_google_login_success": MessageLookupByLibrary.simpleMessage(
+      "Google-Anmeldung erfolgreich",
+    ),
+    "auth_kakao_login": MessageLookupByLibrary.simpleMessage("Kakao-Login"),
+    "auth_kakao_login_failure": m1,
+    "auth_kakao_login_success": MessageLookupByLibrary.simpleMessage(
+      "Kakao-Anmeldung erfolgreich",
+    ),
+    "auth_palnner": MessageLookupByLibrary.simpleMessage("Planer."),
+    "auth_pick_best": MessageLookupByLibrary.simpleMessage(
+      "wählte den besten ",
+    ),
+    "auth_with_timebox_planner": MessageLookupByLibrary.simpleMessage(
+      "treten Sie dem TimeBox Planner bei.",
+    ),
+    "cancel": MessageLookupByLibrary.simpleMessage("Abbrechen"),
+    "delete": MessageLookupByLibrary.simpleMessage("Löschen"),
+    "end": MessageLookupByLibrary.simpleMessage("Ende"),
+    "end_time": MessageLookupByLibrary.simpleMessage("Endzeit"),
+    "exit": MessageLookupByLibrary.simpleMessage("Beenden"),
+    "home_activity_placeholder": MessageLookupByLibrary.simpleMessage(
+      "Geben Sie eine Aktivität ein",
+    ),
+    "home_arrange_title": MessageLookupByLibrary.simpleMessage(
+      "Ansichtsoptionen",
+    ),
+    "home_braindump_placeholder": MessageLookupByLibrary.simpleMessage(
+      "Schreiben Sie alle Gedanken auf, die Ihnen gerade durch den Kopf gehen!",
+    ),
+    "home_save_completed": MessageLookupByLibrary.simpleMessage(
+      "Speichern abgeschlossen",
+    ),
+    "home_save_completed_desc": MessageLookupByLibrary.simpleMessage(
+      "Ihre Daten wurden erfolgreich gespeichert.",
+    ),
+    "home_save_desc": MessageLookupByLibrary.simpleMessage("Speichern"),
+    "home_tig_mode_prepare": MessageLookupByLibrary.simpleMessage(
+      "Daten für den Tig-Modus werden vorbereitet.\nWährend der Vorbereitung wird eine Anzeige gezeigt.\nBitte warten Sie 🙏",
+    ),
+    "main_restart": m2,
+    "menu_contact_us": MessageLookupByLibrary.simpleMessage(
+      "Kontaktieren Sie uns",
+    ),
+    "menu_delete_user_failure": MessageLookupByLibrary.simpleMessage(
+      "Ein Fehler ist aufgetreten.\nBitte versuchen Sie es später erneut.",
+    ),
+    "menu_email_body": MessageLookupByLibrary.simpleMessage(
+      "Es wäre hilfreich, wenn Sie uns Folgendes mitteilen könnten:\nVerwendetes Gerät:\nProblem oder Verbesserungsvorschlag:\n\nFügen Sie gerne weitere Fragen hinzu :)",
+    ),
+    "menu_email_subject": MessageLookupByLibrary.simpleMessage(
+      "Ich habe eine Frage",
+    ),
+    "menu_logout_content": MessageLookupByLibrary.simpleMessage(
+      "Möchten Sie sich abmelden?",
+    ),
+    "menu_logout_text": MessageLookupByLibrary.simpleMessage("Abmelden"),
+    "menu_logout_title": MessageLookupByLibrary.simpleMessage(
+      "Abmeldeanleitung",
+    ),
+    "menu_month_tigs": m3,
+    "menu_price_per_month": m4,
+    "menu_subscribe": MessageLookupByLibrary.simpleMessage("Abonnieren"),
+    "menu_subscribe_cancel": MessageLookupByLibrary.simpleMessage(
+      "Abo kündigen",
+    ),
+    "menu_subscribe_get1": MessageLookupByLibrary.simpleMessage(
+      "Durch ein Abo werden alle Anzeigen entfernt 😊",
+    ),
+    "menu_subscribe_get2": MessageLookupByLibrary.simpleMessage(
+      "Sie können Widgets verwenden, um Ihren monatlichen und täglichen Fortschritt zu verfolgen 😊",
+    ),
+    "menu_subscribe_get3": MessageLookupByLibrary.simpleMessage(
+      "Sie unterstützen die Entwickler bei der Verbesserung der App 😊",
+    ),
+    "menu_subscribe_lose1": MessageLookupByLibrary.simpleMessage(
+      "Wenn Sie das Abo kündigen, werden die Anzeigen wieder erscheinen 🥲",
+    ),
+    "menu_subscribe_lose2": MessageLookupByLibrary.simpleMessage(
+      "Sie verlieren das Widget, das Ihren Fortschritt verfolgt 🥲",
+    ),
+    "menu_subscribe_lose3": MessageLookupByLibrary.simpleMessage(
+      "Sie verlieren den Zugriff auf kommende Funktionen 🥲",
+    ),
+    "menu_update_intro": MessageLookupByLibrary.simpleMessage(
+      "Demnächst verfügbar.",
+    ),
+    "menu_withdrawal_content": MessageLookupByLibrary.simpleMessage(
+      "Alle Ihre Daten werden gelöscht.\nMöchten Sie fortfahren?",
+    ),
+    "menu_withdrawal_text": MessageLookupByLibrary.simpleMessage(
+      "Konto löschen",
+    ),
+    "menu_withdrawal_title": MessageLookupByLibrary.simpleMessage(
+      "Kontolöschung",
+    ),
+    "next": MessageLookupByLibrary.simpleMessage("Weiter"),
+    "ok": MessageLookupByLibrary.simpleMessage("OK"),
+    "option_explain_twelve": MessageLookupByLibrary.simpleMessage(
+      "Im 12-Stunden-Format anzeigen. (1:00 PM ... 12:00 PM)",
+    ),
+    "option_explain_twentyFour": MessageLookupByLibrary.simpleMessage(
+      "Im 24-Stunden-Format anzeigen. (13:00 ... 24:00)",
+    ),
+    "setting": MessageLookupByLibrary.simpleMessage("Einstellungen"),
+    "success": MessageLookupByLibrary.simpleMessage("Erfolg"),
+    "tag_add": MessageLookupByLibrary.simpleMessage("Tag hinzufügen"),
+    "tag_add_input": MessageLookupByLibrary.simpleMessage(
+      "Geben Sie ein Tag ein",
+    ),
+    "tag_delete_completed": m5,
+    "tag_delete_content": MessageLookupByLibrary.simpleMessage(
+      "Sind Sie sicher, dass Sie dieses Tag löschen möchten?",
+    ),
+    "tag_delete_title": MessageLookupByLibrary.simpleMessage(
+      "Löschbestätigung",
+    ),
+    "tag_duplicated": MessageLookupByLibrary.simpleMessage(
+      "Tag kann nicht doppelt hinzugefügt werden.",
+    ),
+    "tag_empty": MessageLookupByLibrary.simpleMessage(
+      "Bitte geben Sie ein Tag ein",
+    ),
+    "tag_title": MessageLookupByLibrary.simpleMessage("Tags"),
+    "tig_mode": MessageLookupByLibrary.simpleMessage("Tig-Modus"),
+    "tig_mode_count_down": m6,
+    "tig_mode_empty_tig": MessageLookupByLibrary.simpleMessage(
+      "Zurzeit gibt es keine Tigs.\nVersuchen Sie, eines hinzuzufügen, und starten Sie erneut 😊",
+    ),
+    "tig_mode_end_time": m7,
+    "tig_mode_remain_time": MessageLookupByLibrary.simpleMessage(
+      "Verbleibende Zeit",
+    ),
+    "tig_mode_start_time": m8,
+    "tig_mode_waiting": MessageLookupByLibrary.simpleMessage("Warten"),
+  };
 }
--- tig/lib/generated/intl/messages_zh_CN.dart
+++ tig/lib/generated/intl/messages_zh_CN.dart
@@ -40,88 +40,107 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "auth_elon_musk": MessageLookupByLibrary.simpleMessage("埃隆·马斯克 "),
-        "auth_for_time_box_planner":
-            MessageLookupByLibrary.simpleMessage("为了实现你的目标，"),
-        "auth_google_login": MessageLookupByLibrary.simpleMessage("谷歌登录"),
-        "auth_google_login_failure": m0,
-        "auth_google_login_success":
-            MessageLookupByLibrary.simpleMessage("谷歌登录成功"),
-        "auth_kakao_login": MessageLookupByLibrary.simpleMessage("Kakao 登录"),
-        "auth_kakao_login_failure": m1,
-        "auth_kakao_login_success":
-            MessageLookupByLibrary.simpleMessage("Kakao 登录成功"),
-        "auth_palnner": MessageLookupByLibrary.simpleMessage("计划工具。"),
-        "auth_pick_best": MessageLookupByLibrary.simpleMessage("选择了最好的 "),
-        "auth_with_timebox_planner":
-            MessageLookupByLibrary.simpleMessage("加入TimeBox Planner吧。"),
-        "cancel": MessageLookupByLibrary.simpleMessage("取消"),
-        "delete": MessageLookupByLibrary.simpleMessage("删除"),
-        "end": MessageLookupByLibrary.simpleMessage("结束"),
-        "end_time": MessageLookupByLibrary.simpleMessage("结束时间"),
-        "exit": MessageLookupByLibrary.simpleMessage("退出"),
-        "home_activity_placeholder":
-            MessageLookupByLibrary.simpleMessage("请写下您的待办事项。"),
-        "home_arrange_title": MessageLookupByLibrary.simpleMessage("查看选项"),
-        "home_braindump_placeholder":
-            MessageLookupByLibrary.simpleMessage("写下您想到的所有想法！"),
-        "home_save_completed": MessageLookupByLibrary.simpleMessage("保存完成"),
-        "home_save_completed_desc":
-            MessageLookupByLibrary.simpleMessage("保存已完成。"),
-        "home_save_desc": MessageLookupByLibrary.simpleMessage("保存"),
-        "home_tig_mode_prepare": MessageLookupByLibrary.simpleMessage(
-            "正在为提格模式准备数据。\n准备过程中会显示广告。\n请稍等 🙏"),
-        "main_restart": m2,
-        "menu_contact_us": MessageLookupByLibrary.simpleMessage("联系我们"),
-        "menu_delete_user_failure":
-            MessageLookupByLibrary.simpleMessage("发生错误。\n请稍后重试。"),
-        "menu_email_body": MessageLookupByLibrary.simpleMessage(
-            "请提供以下信息以便我们帮助您。\n使用的设备：\n错误或改进意见：\n\n如果您有其他问题，请随时联系：)"),
-        "menu_email_subject": MessageLookupByLibrary.simpleMessage("咨询请求"),
-        "menu_logout_content": MessageLookupByLibrary.simpleMessage("您确定要退出吗？"),
-        "menu_logout_text": MessageLookupByLibrary.simpleMessage("注销"),
-        "menu_logout_title": MessageLookupByLibrary.simpleMessage("注销说明"),
-        "menu_month_tigs": m3,
-        "menu_price_per_month": m4,
-        "menu_subscribe": MessageLookupByLibrary.simpleMessage("订阅"),
-        "menu_subscribe_cancel": MessageLookupByLibrary.simpleMessage("取消订阅"),
-        "menu_subscribe_get1":
-            MessageLookupByLibrary.simpleMessage("订阅后，屏幕上的所有广告将被删除😊"),
-        "menu_subscribe_get2":
-            MessageLookupByLibrary.simpleMessage("订阅后，您将获得一个小部件，以查看每月和每日的进度😊"),
-        "menu_subscribe_get3":
-            MessageLookupByLibrary.simpleMessage("订阅将支持开发者，帮助我们创造更好的应用😊"),
-        "menu_subscribe_lose1":
-            MessageLookupByLibrary.simpleMessage("如果您取消订阅，广告将重新出现在屏幕上🥲"),
-        "menu_subscribe_lose2":
-            MessageLookupByLibrary.simpleMessage("如果您取消订阅，您将不再获得查看进度的小部件🥲"),
-        "menu_subscribe_lose3":
-            MessageLookupByLibrary.simpleMessage("如果您取消订阅，您将无法使用将来创建的功能🥲"),
-        "menu_update_intro": MessageLookupByLibrary.simpleMessage("即将推出"),
-        "menu_withdrawal_content": MessageLookupByLibrary.simpleMessage(
-            "您取消会员资格时，所有信息将被删除。\n您是否确定要继续？"),
-        "menu_withdrawal_text": MessageLookupByLibrary.simpleMessage("取消会员资格"),
-        "menu_withdrawal_title": MessageLookupByLibrary.simpleMessage("退会信息"),
-        "next": MessageLookupByLibrary.simpleMessage("下一个"),
-        "ok": MessageLookupByLibrary.simpleMessage("确认"),
-        "setting": MessageLookupByLibrary.simpleMessage("设置"),
-        "success": MessageLookupByLibrary.simpleMessage("成功"),
-        "tag_add": MessageLookupByLibrary.simpleMessage("添加标签"),
-        "tag_add_input": MessageLookupByLibrary.simpleMessage("请输入要添加的标签。"),
-        "tag_delete_completed": m5,
-        "tag_delete_content":
-            MessageLookupByLibrary.simpleMessage("您确定要删除此标签吗？"),
-        "tag_delete_title": MessageLookupByLibrary.simpleMessage("删除说明"),
-        "tag_duplicated": MessageLookupByLibrary.simpleMessage("不能添加相同的标签。"),
-        "tag_empty": MessageLookupByLibrary.simpleMessage("请输入要添加的标签。"),
-        "tag_title": MessageLookupByLibrary.simpleMessage("标签"),
-        "tig_mode": MessageLookupByLibrary.simpleMessage("提格模式"),
-        "tig_mode_count_down": m6,
-        "tig_mode_empty_tig":
-            MessageLookupByLibrary.simpleMessage("当前没有提格。\n请注册提格并重新启动😊"),
-        "tig_mode_end_time": m7,
-        "tig_mode_remain_time": MessageLookupByLibrary.simpleMessage("剩余时间"),
-        "tig_mode_start_time": m8,
-        "tig_mode_waiting": MessageLookupByLibrary.simpleMessage("等待中")
-      };
+    "auth_elon_musk": MessageLookupByLibrary.simpleMessage("埃隆·马斯克 "),
+    "auth_for_time_box_planner": MessageLookupByLibrary.simpleMessage(
+      "为了实现你的目标，",
+    ),
+    "auth_google_login": MessageLookupByLibrary.simpleMessage("谷歌登录"),
+    "auth_google_login_failure": m0,
+    "auth_google_login_success": MessageLookupByLibrary.simpleMessage("谷歌登录成功"),
+    "auth_kakao_login": MessageLookupByLibrary.simpleMessage("Kakao 登录"),
+    "auth_kakao_login_failure": m1,
+    "auth_kakao_login_success": MessageLookupByLibrary.simpleMessage(
+      "Kakao 登录成功",
+    ),
+    "auth_palnner": MessageLookupByLibrary.simpleMessage("计划工具。"),
+    "auth_pick_best": MessageLookupByLibrary.simpleMessage("选择了最好的 "),
+    "auth_with_timebox_planner": MessageLookupByLibrary.simpleMessage(
+      "加入TimeBox Planner吧。",
+    ),
+    "cancel": MessageLookupByLibrary.simpleMessage("取消"),
+    "delete": MessageLookupByLibrary.simpleMessage("删除"),
+    "end": MessageLookupByLibrary.simpleMessage("结束"),
+    "end_time": MessageLookupByLibrary.simpleMessage("结束时间"),
+    "exit": MessageLookupByLibrary.simpleMessage("退出"),
+    "home_activity_placeholder": MessageLookupByLibrary.simpleMessage(
+      "请写下您的待办事项。",
+    ),
+    "home_arrange_title": MessageLookupByLibrary.simpleMessage("查看选项"),
+    "home_braindump_placeholder": MessageLookupByLibrary.simpleMessage(
+      "写下您想到的所有想法！",
+    ),
+    "home_save_completed": MessageLookupByLibrary.simpleMessage("保存完成"),
+    "home_save_completed_desc": MessageLookupByLibrary.simpleMessage("保存已完成。"),
+    "home_save_desc": MessageLookupByLibrary.simpleMessage("保存"),
+    "home_tig_mode_prepare": MessageLookupByLibrary.simpleMessage(
+      "正在为提格模式准备数据。\n准备过程中会显示广告。\n请稍等 🙏",
+    ),
+    "main_restart": m2,
+    "menu_contact_us": MessageLookupByLibrary.simpleMessage("联系我们"),
+    "menu_delete_user_failure": MessageLookupByLibrary.simpleMessage(
+      "发生错误。\n请稍后重试。",
+    ),
+    "menu_email_body": MessageLookupByLibrary.simpleMessage(
+      "请提供以下信息以便我们帮助您。\n使用的设备：\n错误或改进意见：\n\n如果您有其他问题，请随时联系：)",
+    ),
+    "menu_email_subject": MessageLookupByLibrary.simpleMessage("咨询请求"),
+    "menu_logout_content": MessageLookupByLibrary.simpleMessage("您确定要退出吗？"),
+    "menu_logout_text": MessageLookupByLibrary.simpleMessage("注销"),
+    "menu_logout_title": MessageLookupByLibrary.simpleMessage("注销说明"),
+    "menu_month_tigs": m3,
+    "menu_price_per_month": m4,
+    "menu_subscribe": MessageLookupByLibrary.simpleMessage("订阅"),
+    "menu_subscribe_cancel": MessageLookupByLibrary.simpleMessage("取消订阅"),
+    "menu_subscribe_get1": MessageLookupByLibrary.simpleMessage(
+      "订阅后，屏幕上的所有广告将被删除😊",
+    ),
+    "menu_subscribe_get2": MessageLookupByLibrary.simpleMessage(
+      "订阅后，您将获得一个小部件，以查看每月和每日的进度😊",
+    ),
+    "menu_subscribe_get3": MessageLookupByLibrary.simpleMessage(
+      "订阅将支持开发者，帮助我们创造更好的应用😊",
+    ),
+    "menu_subscribe_lose1": MessageLookupByLibrary.simpleMessage(
+      "如果您取消订阅，广告将重新出现在屏幕上🥲",
+    ),
+    "menu_subscribe_lose2": MessageLookupByLibrary.simpleMessage(
+      "如果您取消订阅，您将不再获得查看进度的小部件🥲",
+    ),
+    "menu_subscribe_lose3": MessageLookupByLibrary.simpleMessage(
+      "如果您取消订阅，您将无法使用将来创建的功能🥲",
+    ),
+    "menu_update_intro": MessageLookupByLibrary.simpleMessage("即将推出"),
+    "menu_withdrawal_content": MessageLookupByLibrary.simpleMessage(
+      "您取消会员资格时，所有信息将被删除。\n您是否确定要继续？",
+    ),
+    "menu_withdrawal_text": MessageLookupByLibrary.simpleMessage("取消会员资格"),
+    "menu_withdrawal_title": MessageLookupByLibrary.simpleMessage("退会信息"),
+    "next": MessageLookupByLibrary.simpleMessage("下一个"),
+    "ok": MessageLookupByLibrary.simpleMessage("确认"),
+    "option_explain_twelve": MessageLookupByLibrary.simpleMessage(
+      "以12小时制显示。(1:00 PM ... 12:00 PM)",
+    ),
+    "option_explain_twentyFour": MessageLookupByLibrary.simpleMessage(
+      "以24小时制显示。(13:00 ... 24:00)",
+    ),
+    "setting": MessageLookupByLibrary.simpleMessage("设置"),
+    "success": MessageLookupByLibrary.simpleMessage("成功"),
+    "tag_add": MessageLookupByLibrary.simpleMessage("添加标签"),
+    "tag_add_input": MessageLookupByLibrary.simpleMessage("请输入要添加的标签。"),
+    "tag_delete_completed": m5,
+    "tag_delete_content": MessageLookupByLibrary.simpleMessage("您确定要删除此标签吗？"),
+    "tag_delete_title": MessageLookupByLibrary.simpleMessage("删除说明"),
+    "tag_duplicated": MessageLookupByLibrary.simpleMessage("不能添加相同的标签。"),
+    "tag_empty": MessageLookupByLibrary.simpleMessage("请输入要添加的标签。"),
+    "tag_title": MessageLookupByLibrary.simpleMessage("标签"),
+    "tig_mode": MessageLookupByLibrary.simpleMessage("提格模式"),
+    "tig_mode_count_down": m6,
+    "tig_mode_empty_tig": MessageLookupByLibrary.simpleMessage(
+      "当前没有提格。\n请注册提格并重新启动😊",
+    ),
+    "tig_mode_end_time": m7,
+    "tig_mode_remain_time": MessageLookupByLibrary.simpleMessage("剩余时间"),
+    "tig_mode_start_time": m8,
+    "tig_mode_waiting": MessageLookupByLibrary.simpleMessage("等待中"),
+  };
 }
